35c77ed92dd7944ad5060b84d336e40155aceb0b
Angelo Bonini <abonini2000@gmail.com>
2021-09-07 19:59:26 -0300

    reducers handling own states

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,14 @@
-import * as fromStore from "./store/store";
+import * as fromStore from "./store/index";
 import { renderTodos } from "./utils";
 
 const input = document.querySelector("input") as HTMLInputElement;
 const button = document.querySelector("button") as HTMLButtonElement;
 const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
 const todoList = document.querySelector(".todos") as HTMLLIElement;
-
-const store = new fromStore.Store(
-  {},
-  {
-    todos: [{ label: "Eat sandwich", complete: false }],
-  }
-);
-
+const reducers = {
+  todos: fromStore.reducer,
+};
+const store = new fromStore.Store(reducers);
 
 button.addEventListener(
   "click",
@@ -22,10 +18,10 @@ button.addEventListener(
     const payload = { label: input.value, complete: false };
 
     store.dispatch({
-      type: 'ADD_TODO',
-      payload
-    })
-
+      type: "ADD_TODO",
+      payload,
+    });
+    console.log(store.value);
     input.value = "";
   },
   false
